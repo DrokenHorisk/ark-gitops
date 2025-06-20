41023063668a7e2a8ee50febedca0a70877455eb
Droken Horisk <droken.horisk@gmail.com>
2025-06-21 00:36:57 +0200

    Mise à jour du script Dockerfile pour ajouter les permissions d'exécution à SteamCMD et modification du script de démarrage pour éviter les erreurs X en utilisant Xvfb.

--- docker/ark-wine-server/start.sh
+++ docker/ark-wine-server/start.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 echo ">> Mise à jour du serveur ASA..."
 
+chmod +x /steamcmd/steamcmd.sh
 /steamcmd/steamcmd.sh +force_install_dir /usr/games/ark +login anonymous +app_update 2430930 validate +quit
 
 echo ">> Lancement du serveur ASA..."
 cd /usr/games/ark
-export DISPLAY=none  # Évite les erreurs X
-wine ASA.exe -ServerMap=TheIsland -log
+
+# Empêche Wine de tenter de créer une fenêtre
+export DISPLAY=:99
+Xvfb :99 -screen 0 1024x768x16 &
+
+wine ASA.exe -ServerMap=${MAP_NAME:-TheIsland} -log
