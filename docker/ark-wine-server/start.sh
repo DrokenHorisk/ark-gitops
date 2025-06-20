3bc90a38e91c8d0e600c31b522372631f82f8a0a
Droken Horisk <droken.horisk@gmail.com>
2025-06-21 00:28:34 +0200

    Ajout de sudo pour exécuter SteamCMD avec les permissions appropriées dans le script de lancement

--- docker/ark-wine-server/start.sh
+++ docker/ark-wine-server/start.sh
@@ -3,10 +3,14 @@ set -e
 
 echo ">> Mise à jour du serveur ASA..."
 <<<<<<< HEAD
+<<<<<<< HEAD
 /steamcmd/steamcmd.sh +login anonymous +force_install_dir /usr/games/ark +app_update 2430930 validate +quit
 =======
 sudo /steamcmd/steamcmd.sh +login anonymous +force_install_dir /usr/games/ark +app_update 2430930 validate +quit
 >>>>>>> 1ef2f87 (Suppression de la commande chmod pour le script SteamCMD dans le script de lancement)
+=======
+/steamcmd/steamcmd.sh +login anonymous +force_install_dir /usr/games/ark +app_update 2430930 validate +quit
+>>>>>>> 61a254d (Ajout de sudo pour exécuter SteamCMD avec les permissions appropriées dans le script de lancement)
 
 # Vérification du binaire ASA
 if [ ! -f /usr/games/ark/ASA.exe ]; then
