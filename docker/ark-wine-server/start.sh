6cd3163064b4ee8ebcc350f4e4e573cd32e80c22
Droken Horisk <droken.horisk@gmail.com>
2025-06-21 00:35:13 +0200

    Amélioration de l'installation de SteamCMD et mise à jour du script de démarrage pour éviter les erreurs X

--- docker/ark-wine-server/start.sh
+++ docker/ark-wine-server/start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo ">> Mise à jour du serveur ASA..."
 
-# Exécute steamcmd sans changer ses permissions
 /steamcmd/steamcmd.sh +force_install_dir /usr/games/ark +login anonymous +app_update 2430930 validate +quit
 
 echo ">> Lancement du serveur ASA..."
 cd /usr/games/ark
+export DISPLAY=none  # Évite les erreurs X
 wine ASA.exe -ServerMap=TheIsland -log
