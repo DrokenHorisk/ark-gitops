7cddab6bac49ccb282b3bbf2de6a7112808032cf
Droken Horisk <droken.horisk@gmail.com>
2025-06-21 00:26:36 +0200

    Suppression de la commande chmod pour le script SteamCMD dans le script de lancement

--- docker/ark-wine-server/start.sh
+++ docker/ark-wine-server/start.sh
@@ -2,7 +2,11 @@
 set -e
 
 echo ">> Mise à jour du serveur ASA..."
+<<<<<<< HEAD
 /steamcmd/steamcmd.sh +login anonymous +force_install_dir /usr/games/ark +app_update 2430930 validate +quit
+=======
+sudo /steamcmd/steamcmd.sh +login anonymous +force_install_dir /usr/games/ark +app_update 2430930 validate +quit
+>>>>>>> 1ef2f87 (Suppression de la commande chmod pour le script SteamCMD dans le script de lancement)
 
 # Vérification du binaire ASA
 if [ ! -f /usr/games/ark/ASA.exe ]; then
