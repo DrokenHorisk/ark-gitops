e70e571f5662e329e8e366b1b23cfb2804510df2
Droken Horisk <droken.horisk@gmail.com>
2025-06-21 11:01:51 +0200

    Amélioration du Dockerfile et du script de démarrage : réorganisation des sections, ajout de la création du répertoire d'exécution pour Wine, et correction de la commande de mise à jour ARK.

--- docker/ark-wine-server/start.sh
+++ docker/ark-wine-server/start.sh
@@ -10,7 +10,7 @@ sleep 2
 export DISPLAY=:0
 
 # Optionnel : mise à jour ARK
-#/steamcmd/steamcmd.sh +login anonymous +force_install_dir /usr/games/ark +app_update 2430930 validate +quit
+/steamcmd/steamcmd.sh +login anonymous +force_install_dir /usr/games/ark +app_update 2430930 validate +quit
 
 # Lancer le serveur
 wine /usr/games/ark/ShooterGame/Binaries/Win64/ShooterGameServer.exe TheIsland?SessionName="ARK NAS"?MaxPlayers=10?listen -server -log
